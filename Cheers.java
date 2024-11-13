50dacb7a95cc7de5a897461c2d43071893e36623
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-13 16:18:19 +0200

    Update Cheers.java

--- Cheers.java
+++ Cheers.java
@@ -9,7 +9,7 @@ public class Cheers {
                 System.out.print("Give me ");
                 boolean IsAn = (crowdupper.indexOf('A')==i || crowdupper.indexOf('E')==i || crowdupper.indexOf('F')==i || crowdupper.indexOf('H')==i|| crowdupper.indexOf('I')==i || crowdupper.indexOf('L')==i || crowdupper.indexOf('M')==i|| crowdupper.indexOf('N')==i || crowdupper.indexOf('O')==i || crowdupper.indexOf('R')==i || crowdupper.indexOf('S')==i || crowdupper.indexOf('X')==i) ;
                 if (IsAn) {System.out.println("an "+ crowdupper.charAt(i) + ": "+ crowdupper.charAt(i) + "!");}
-                else {System.out.println("a "+ crowdupper.charAt(i) + ": "+ crowdupper.charAt(i) + "!");}
+                else {System.out.println("a  "+ crowdupper.charAt(i) + ": "+ crowdupper.charAt(i) + "!");}
             }
             System.out.println("What does that spell?");
             int times = Integer.parseInt(args[1]);
